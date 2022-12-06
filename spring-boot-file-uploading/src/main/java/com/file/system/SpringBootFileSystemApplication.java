5df204004d2136c88c615eda13df9547f3fa4df3
TejasKute151200 <113657741+TejasKute151200@users.noreply.github.com>
2022-12-06 19:48:30 +0530

    Add files via upload

---
+++ spring-boot-file-uploading/src/main/java/com/file/system/SpringBootFileSystemApplication.java
@@ -0,0 +1,13 @@
+package com.file.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFileSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFileSystemApplication.class, args);
+	}
+
+}
---
+++ spring-boot-file-uploading/src/main/java/com/file/system/service/FileService.java
@@ -0,0 +1,31 @@
+package com.file.system.service;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import com.file.system.entity.FileSystem;
+import com.file.system.repo.FileRepo;
+
+@Service
+public class FileService {
+
+	@Autowired
+	private FileRepo repo;
+
+	public FileSystem store(MultipartFile file) throws IOException {
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+		FileSystem fileSystem = new FileSystem(fileName, file.getContentType(), file.getBytes());
+		return repo.save(fileSystem);
+	}
+
+	public FileSystem getFile(String id) {
+		return repo.findById(id).get();
+	}
+
+	public Stream<FileSystem>getAllFiles(){
+			return repo.findAll().stream();
+	}
+}
